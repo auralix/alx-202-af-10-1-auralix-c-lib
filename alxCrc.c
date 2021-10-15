3dd76a84916216b96a8c773e2e4b4815447b5225
Tomaz Vidovic <tomaz@tomazvidovic.com>
2021-10-15 21:44:31 +0200

    Added empty CRC implemtations

--- alxCrc.c
+++ alxCrc.c
@@ -14,5 +14,26 @@
 
 
 //******************************************************************************
+// Constructor
+//******************************************************************************
+void AlxCrc_Ctor
+(
+	AlxCrc* me,
+	AlxCrc_Config config
+)
+{
+}
+
+
+//******************************************************************************
 // Functions
 //******************************************************************************
+uint32_t AlxCrc_Calc(AlxCrc* me, uint8_t* data, uint32_t len)
+{
+}
+bool AlxCrc_IsOk(AlxCrc* me, uint8_t* dataWithCrc, uint32_t lenWithCrc, uint32_t* validatedCrc)
+{
+}
+uint32_t AlxCrc_GetLen(AlxCrc* me)
+{
+}
