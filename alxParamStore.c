b4386bb95d5343807505c4f079086122b3199d27
Jure Kolsek <jure.kolsek@auralix.eu>
2021-10-28 19:07:02 +0200

    Correcred alxCrc, added functions for 1xGroup to alxParamStore.

--- alxParamStore.c
+++ alxParamStore.c
@@ -17,6 +17,7 @@
 // Private Functions
 //******************************************************************************
 static bool AlxParamStore_IsParamGroupInitOk(AlxParamStore* me);
+static void AlxParamStore_Handle_1xGroup(AlxParamStore* me);
 static void AlxParamStore_Handle_2xGroup(AlxParamStore* me);
 static void AlxParamStore_Handle_4xGroup(AlxParamStore* me);
 
@@ -86,6 +87,11 @@ void AlxParamStore_Handle(AlxParamStore* me)
 	// #2 Config
 	switch (me->numOfParamGroups)
 	{
+		case 1:
+		{
+			AlxParamStore_Handle_1xGroup(me);
+			break;
+		}
 		case 2:
 		{
 			AlxParamStore_Handle_2xGroup(me);
@@ -108,7 +114,7 @@ bool AlxParamStore_IsErr(AlxParamStore* me)
 	// #1 Assert
 	ALX_PARAM_STORE_ASSERT(me->wasCtorCalled == true);
 	ALX_PARAM_STORE_ASSERT(me->isInit == true);
-	
+
 	// #2 Return
 	if (me->st == AlxParamStore_St_Err)
 		return true;
@@ -123,6 +129,100 @@ bool AlxParamStore_IsErr(AlxParamStore* me)
 static bool AlxParamStore_IsParamGroupInitOk(AlxParamStore* me)
 {
 	return !(me->isParamGroupInitErr);
+}
+static void AlxParamStore_Handle_1xGroup(AlxParamStore* me)
+{
+	switch (me->st)
+	{
+	case AlxParamStore_St_Init:
+		{
+			// #1 Transition
+			if(AlxParamStore_IsParamGroupInitOk(me))
+			{
+				// #1.1 Change state
+				me->st = AlxParamStore_St_CheckingGroup1;
+
+				// #1.2 Trace
+				ALX_PARAM_STORE_TRACE("InitOk");
+			}
+
+			// #2 Do Nothing
+
+			// #3 Break
+			break;
+		}
+	case AlxParamStore_St_Err:
+		{
+			// #1 Do Nothing
+
+			// #2 Break
+			break;
+		}
+	case AlxParamStore_St_CheckingGroup1:
+		{
+			// #1 Transition
+			if(AlxParamGroup_IsValStoredBuffDiff(me->paramGroups[0]))
+			{
+				// #1.1 Prepare values to store
+				AlxParamGroup_ValBuffToValToStoreBuff(me->paramGroups[0]);
+
+				// #1.2 Start writing
+				AlxParamGroup_Write(me->paramGroups[0]);
+
+				// #1.3 Change state
+				me->st = AlxParamStore_St_WritingGroup1;
+
+				// #1.4 Trace
+				ALX_PARAM_STORE_TRACE("Group1_WriteStart");
+			}
+
+			// #2.1 Refresh values
+			AlxParamGroup_ParamItemsValToValBuff(me->paramGroups[0]);
+
+			// #3 Do Nothing
+
+			// #4 Break
+			break;
+		}
+	case AlxParamStore_St_WritingGroup1:
+		{
+			// #1 Transition
+			if(AlxParamGroup_IsWriteDone(me->paramGroups[0]))
+			{
+				// #1.1 Update stored values
+				AlxParamGroup_ValToStoreBuffToValStoredBuff(me->paramGroups[0]);
+
+				// #1.2 Change state
+				me->st = AlxParamStore_St_CheckingGroup1;
+
+				// #1.3 Trace
+				ALX_PARAM_STORE_TRACE("Group1_WriteDone");
+			}
+
+			// #2 Transition
+			else if(AlxParamGroup_IsWriteErr(me->paramGroups[0]))
+			{
+				// #2.1 Change state
+				me->st = AlxParamStore_St_Err;
+
+				// #2.2 Trace
+				ALX_PARAM_STORE_TRACE("Group1_Err");
+			}
+
+			// #3 Do Nothing
+
+			// #4 Break
+			break;
+		}
+	default:
+		{
+			// #1 Assert
+			ALX_PARAM_STORE_ASSERT(false); 	// We should never get here
+
+			// #2 Break
+			break;
+		}
+	}
 }
 static void AlxParamStore_Handle_2xGroup(AlxParamStore* me)
 {
@@ -174,7 +274,7 @@ static void AlxParamStore_Handle_2xGroup(AlxParamStore* me)
 			{
 				// #2.1 Refresh values
 				AlxParamGroup_ParamItemsValToValBuff(me->paramGroups[0]);
-				
+
 				// #2.2 Change state
 				me->st = AlxParamStore_St_CheckingGroup2;
 			}
@@ -236,7 +336,7 @@ static void AlxParamStore_Handle_2xGroup(AlxParamStore* me)
 			{
 				// #2.1 Refresh values
 				AlxParamGroup_ParamItemsValToValBuff(me->paramGroups[1]);
-				
+
 				// #2.2 Change state
 				me->st = AlxParamStore_St_CheckingGroup1;
 			}
