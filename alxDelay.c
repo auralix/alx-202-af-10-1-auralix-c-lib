96b8491537faa107e397295883d3154c7f70912f
martinflisar <martin.flisar@auralix.eu>
2022-03-09 23:57:49 +0100

    Implement FreeRtos delay to alxDelay module

--- alxDelay.c
+++ alxDelay.c
@@ -18,11 +18,50 @@
 //******************************************************************************
 void AlxDelay_ns(uint64_t delay_ns)
 {
+	#if defined(ALX_FREE_RTOS)
+	// We should not get here	// MF: FreeRtos lowest delay unit is 1ms
+	#else
 	uint64_t ticksStart_ns = AlxTick_Get_ns(&alxTick);
 	while ((AlxTick_Get_ns(&alxTick) - ticksStart_ns) < delay_ns);
+	#endif
 }
-void AlxDelay_us(uint64_t delay_us)		{ AlxDelay_ns(delay_us  * 1000); }
-void AlxDelay_ms(uint64_t delay_ms)		{ AlxDelay_ns(delay_ms  * 1000000); }
-void AlxDelay_sec(uint64_t delay_sec)	{ AlxDelay_ns(delay_sec * 1000000000); }
-void AlxDelay_min(uint64_t delay_min)	{ AlxDelay_ns(delay_min * 60000000000); }
-void AlxDelay_hr(uint64_t delay_hr)		{ AlxDelay_ns(delay_hr  * 3600000000000); }
+void AlxDelay_us(uint64_t delay_us)
+{
+	#if defined(ALX_FREE_RTOS)
+	// We should not get here	// MF: FreeRtos lowest delay unit is 1ms
+	#else
+	AlxDelay_ns(delay_us * 1000);
+	#endif
+}
+void AlxDelay_ms(uint64_t delay_ms)
+{
+	#if defined(ALX_FREE_RTOS)
+	vTaskDelay(delay_ms * 1 * configTICK_RATE_HZ / 1000);
+	#else
+	AlxDelay_ns(delay_ms * 1000000);
+	#endif
+}
+void AlxDelay_sec(uint64_t delay_sec)
+{
+	#if defined(ALX_FREE_RTOS)
+	vTaskDelay(delay_sec * 1000 * configTICK_RATE_HZ / 1000);
+	#else
+	AlxDelay_ns(delay_sec * 1000000000);
+	#endif
+}
+void AlxDelay_min(uint64_t delay_min)
+{
+	#if defined(ALX_FREE_RTOS)
+	vTaskDelay(delay_min * 60000 * configTICK_RATE_HZ / 1000);
+	#else
+	AlxDelay_ns(delay_min * 60000000000);
+	#endif
+}
+void AlxDelay_hr(uint64_t delay_hr)
+{
+	#if defined(ALX_FREE_RTOS)
+	vTaskDelay(delay_hr * 3600000 * configTICK_RATE_HZ / 1000);
+	#else
+	AlxDelay_ns(delay_hr * 3600000000000);
+	#endif
+}
