e3cf1924aaea9b3f607782f215bbb1d76117e9c3
martinflisar <martin.flisar@auralix.eu>
2022-04-02 19:41:07 +0200

    Finished writing all registers for alxCrn120.

--- Ext/alxCrn120.h
+++ Ext/alxCrn120.h
@@ -50,6 +50,46 @@ extern "C" {
 //******************************************************************************
 // Register Values Unions
 //******************************************************************************
+// General Enums //
+typedef enum
+{
+	NcReg_TransferDir_FromI2cToNfc = 0b0,
+	NcReg_TransferDir_FromNfcToI2c = 0b1
+} AlxCrn120_CfgSess_NcReg_TransferDir;
+typedef enum
+{
+	NcReg_SramMirror_Disable = 0b0,
+	NcReg_SramMirror_Enable  = 0b1
+} AlxCrn120_CfgSess_NcReg_SramMirror;
+typedef enum
+{
+	NcReg_EdOn_RFU        = 0b00,
+	NcReg_EdOn_Soc        = 0b01,
+	NcReg_EdOn_TagSelect  = 0b10,
+	NcReg_EdOn_ReadToRead = 0b11
+} AlxCrn120_CfgSess_NcReg_EdOn;
+typedef enum			// MF: TODO Imena fukajo, nevem, neznam, ne morem
+{
+	NcReg_EdOff_00 = 0b00,	// if the field is switched off
+	NcReg_EdOff_01 = 0b01,	// if the field is switched off or the tag is set to the HALT state
+	NcReg_EdOff_10 = 0b10,	// if the field is switched off or the last page of the NDEF message has been read (defined in LAST_NDEF_BLOCK)
+	NcReg_EdOff_11 = 0b11	// (if ED_ON = 11b) if the field is switched off or if last data is read by I2C(in pass - through mode NFC ---> I2C) or last data is written by I2C(in pass - through mode I2C ---> NFC)
+} AlxCrn120_CfgSess_NcReg_EdOff;
+typedef enum
+{
+	NcReg_PassThroughMode_Disable	= 0b0,
+	NcReg_PassThroughMode_Enable	= 0b1
+} AlxCrn120_CfgSess_NcReg_PassThroughMode;
+typedef enum
+{
+	NcReg_NfcsI2cRst_Off = 0b0,
+	NcReg_NfcsI2cRst_On  = 0b1
+} AlxCrn120_CfgSess_NcReg_NfcsI2cRst;
+typedef enum
+{
+	I2cClockStr_ClockStretching_Disable = 0b0,
+	I2cClockStr_ClockStretching_Enable  = 0b1
+} AlxCrn120_CfgSess_I2cClockStr_ClockStretching;
 
 //------------------------------------------------------------------------------
 // Register 00h
@@ -154,10 +194,16 @@ typedef union
 {
 	struct __attribute__((packed))
 	{
+		// #1 Addr
 		uint8_t	 Addr : 8;
+
+		// #2 Serial number (UID)
 		uint64_t Serial_number : 48;
+
+		// #3 Internal
 		uint32_t Internal : 24;
 
+		// #4 Static lock bytes
 		AlxCrn120_00h_StaticLock_Block_CC BLCC : 1;
 		AlxCrn120_00h_StaticLock_Block_9_4 BL9_14 : 1;
 		AlxCrn120_00h_StaticLock_Block_15_10 BL15_10 : 1;
@@ -175,12 +221,13 @@ typedef union
 		AlxCrn120_00h_StaticLock_Page_14 L14 : 1;
 		AlxCrn120_00h_StaticLock_Page_15 L15 : 1;
 
+		// #5 Capability Container (CC)
 		uint32_t Capability_Container_CC : 32;
 	};
 	uint8_t raw[16];
 } AlxCrn120_RegVal_00h;
 
-//------------------------------------------------------------------------------	// MF Fuka, nevem tocno ce je Protected_user_memory 32bit al 64bit. NFC ma naslove po 4byte, tak da je potemtakem 0xE0 pri NFC enak 0x38 pri I2C, torej bi mogo bit Protected_user_memory 64bit
+//------------------------------------------------------------------------------
 // Register 38h
 //------------------------------------------------------------------------------
 typedef enum
@@ -257,8 +304,11 @@ typedef union
 {
 	struct __attribute__((packed))
 	{
-		uint64_t Protected_user_memory	: 64;
+		// #1 Protected user memory
+		uint64_t Protected_user_memory	: 64;	// MF: Fuka, nevem tocno ce je 32bit al 64bit. NFC ma naslove po 4byte, tak da je potemtakem 0xE0 pri NFC enak 0x38 pri I2C, torej bi moglo bit 64bit
 
+		// #2 Dynamic lock bytes
+		// #2.1 Dynamic Lock Byte 0
 		AlxCrn120_38h_DynamicLock_Page_16_31 L16_31 : 1;
 		AlxCrn120_38h_DynamicLock_Page_32_47 L32_47 : 1;
 		AlxCrn120_38h_DynamicLock_Page_48_63 L48_63 : 1;
@@ -267,18 +317,23 @@ typedef union
 		AlxCrn120_38h_DynamicLock_Page_96_111 L96_111 : 1;
 		AlxCrn120_38h_DynamicLock_Page_112_127 L112_127 : 1;
 		AlxCrn120_38h_DynamicLock_Page_128_143 L128_143 : 1;
+		// #2.2 Dynamic Lock Byte 1
 		AlxCrn120_38h_DynamicLock_Page_144_159 L144_159 : 1;
 		AlxCrn120_38h_DynamicLock_Page_160_175 L160_175 : 1;
 		AlxCrn120_38h_DynamicLock_Page_176_191 L176_191 : 1;
 		AlxCrn120_38h_DynamicLock_Page_192_207 L192_207 : 1;
 		AlxCrn120_38h_DynamicLock_Page_208_223 L208_223 : 1;
 		AlxCrn120_38h_DynamicLock_Page_224_225 L224_225 : 1;
-		bool unused_6_Rfui : 1;
-		bool unused_7_Rfui : 1;
-		uint8_t unused : 8;		// MF: We won't use 2nd Byte of "Dynamic Lock Bytes"
+		uint8_t unused_6_7_RFUI : 2; // bits 6..7 are unused
+		// #2.3 Dynamic Lock Byte 3
+		uint8_t unused : 8;					// MF: We won't use 2nd Byte of "Dynamic Lock Bytes"
+		// #2.4 Dynamic Lock Byte 3
 		uint8_t _00h : 8;
 
+		// #3 RFU
 		uint32_t RFU : 24;
+
+		// #4 AUTH0
 		uint8_t AUTH0 : 8;
 	};
 	uint8_t raw[16];
@@ -287,17 +342,61 @@ typedef union
 //------------------------------------------------------------------------------
 // Register 39h
 //------------------------------------------------------------------------------
+typedef enum
+{
+	Access_Nfc_Prot_Write = 0b0,
+	Access_Nfc_Prot_ReadAndWrite = 0b1
+} AlxCrn120_39h_Access_Nfc_Prot;
+typedef enum
+{
+	Access_AuthLim_Disabled        = 0b000,
+	Access_AuthLim_MaxAttempts_2   = 0b001,
+	Access_AuthLim_MaxAttempts_4   = 0b010,
+	Access_AuthLim_MaxAttempts_8   = 0b011,
+	Access_AuthLim_MaxAttempts_16  = 0b100,
+	Access_AuthLim_MaxAttempts_32  = 0b101,
+	Access_AuthLim_MaxAttempts_64  = 0b110,
+	Access_AuthLim_MaxAttempts_128 = 0b111
+} AlxCrn120_39h_Access_AuthLim;
+typedef enum
+{
+	Pt_I2c_I2cProt_EntireUrsMemAccessible  = 0b00,
+	Pt_I2c_I2cProt_ReadOnlyToProtectedArea = 0b01,
+	Pt_I2c_I2cProt_NoAccessoProtectedArea  = 0b10
+} AlxCrn120_39h_Pt_I2c_I2cProt;
+typedef enum
+{
+	Pt_I2c_SramProt_PwdAuthDisabled = 0b0,
+	Pt_I2c_SramProt_PwdAuthNeeded   = 0b1,
+} AlxCrn120_39h_Pt_I2c_SramProt;
 typedef union
 {
 	struct __attribute__((packed))
 	{
-		uint8_t ACCESS	: 8;
-		uint32_t RFU	: 24;
-		uint32_t PWD	: 24;
+		// #1 ACCESS
+		AlxCrn120_39h_Access_Nfc_Prot NFC_PROT : 1;
+		uint8_t unused_3_6 : 4; // bits 3..6 are unused
+		AlxCrn120_39h_Access_AuthLim AUTHLIM : 3;
+
+		// #2 RFU
+		uint32_t RFU1	: 24;
+
+		// #3 PWD
+		uint32_t PWD	: 32;
+
+		// #4 PACK
 		uint16_t PACK	: 16;
-		uint16_t RFU_	: 16;
-		uint8_t PT_I2C	: 8;
-		uint32_t RFU_2	: 24;
+
+		// #5 RFU
+		uint16_t RFU2	: 16;
+
+		// #6 PT_I2C
+		AlxCrn120_39h_Pt_I2c_I2cProt I2C_PROT : 2;
+		AlxCrn120_39h_Pt_I2c_SramProt SRAM_PROT : 1;
+		uint8_t unused_3_7 : 5; // bits 3..7 are unused
+
+		// #7 RFU
+		uint32_t RFU3	: 24;
 	};
 	uint8_t raw[16];
 } AlxCrn120_RegVal_39h;
@@ -305,19 +404,54 @@ typedef union
 //------------------------------------------------------------------------------
 // Register 3Ah
 //------------------------------------------------------------------------------
+typedef enum
+{
+	Config_RegLock_Nfc_Changeable = 0b0,
+	Config_RegLock_Nfc_Locked     = 0b1
+} AlxCrn120_Config_RegLock_Nfc;
+typedef enum
+{
+	Config_RegLock_I2c_Changeable = 0b0,
+	Config_RegLock_I2c_Locked     = 0b1
+} AlxCrn120_Config_RegLock_I2c;
 typedef union
 {
 	struct __attribute__((packed))
 	{
-		uint8_t NR_REG				: 8;	/* 	NR_REG									R/W		1 byte			*/
-		uint8_t LAST_NDEF_BLOCK		: 8;	/* 	LAST_NDEF_BLOCK							R/W		1 byte			*/
-		uint8_t SRAM_MIRROR_BLOCK	: 8;	/* 	SRAM_MIRROR_BLOCK						R/W		1 byte			*/
-		uint8_t WDT_LS				: 8;	/* 	WDT_LS									R/W		1 byte			*/
-		uint8_t WDT_MS				: 8;	/* 	WDT_MS									R/W		1 byte			*/
-		uint8_t I2C_CLOCK_STR		: 8;	/* 	I2C_CLOCK_STR							R/W		1 byte			*/
-		uint8_t REG_LOCK			: 8;	/* 	REG_LOCK								R/W		1 byte			*/
-		uint8_t RFU					: 8;	/* 	RFU										R/W		1 byte			*/
-		uint64_t _00h				: 64;	/* 	00h										R		8 bytes			*/
+		// #1 NC_REG
+		AlxCrn120_CfgSess_NcReg_TransferDir TRANSFER_DIR : 1;
+		AlxCrn120_CfgSess_NcReg_SramMirror SRAM_MIRROR : 1;
+		AlxCrn120_CfgSess_NcReg_EdOn ED_ON : 2;
+		AlxCrn120_CfgSess_NcReg_EdOff ED_OFF : 2;
+		AlxCrn120_CfgSess_NcReg_PassThroughMode PTHRU : 1;
+		AlxCrn120_CfgSess_NcReg_NfcsI2cRst NFCS_I2C_RST : 1;
+
+		// #2 LAST_NDEF_BLOCK
+		uint8_t LAST_NDEF_BLOCK : 8;
+
+		// #3 SRAM_MIRROR_BLOCK
+		uint8_t SRAM_MIRROR_BLOCK : 8;
+
+		// #4 WDT_LS
+		uint8_t WDT_LS : 8;
+
+		// #5 WDT_MS
+		uint8_t WDT_MS : 8;
+
+		// #6 I2C_CLOCK_STR
+		AlxCrn120_CfgSess_I2cClockStr_ClockStretching I2C_CLOCK_STR : 1;
+		uint8_t unused_1_7_RFU : 7; // bits 1..7 are unused
+
+		// #7 REG_LOCK
+		AlxCrn120_Config_RegLock_Nfc REG_LOCK_NFC : 1;
+		AlxCrn120_Config_RegLock_I2c REG_LOCK_I2C : 1;
+		int8_t unused_2_7_RFU : 7; // bits 2..7 are unused
+
+		// #8 RFU
+		uint8_t RFU : 8;
+
+		// #9 00
+		uint64_t _00h : 64;
 	};
 	uint8_t raw[16];
 } AlxCrn120_RegVal_3Ah_ConfigurationReg;
@@ -326,132 +460,94 @@ typedef union
 // Register FEh
 //------------------------------------------------------------------------------
 typedef enum
-{	/*defines the data flow direction when pass-through mode is enabled*/
-	TransferData_I2C_to_NFC = 0b0,	/*	R/W		0b: from I2C to NFC interface										*/
-	TransferData_NFC_to_I2C = 0b1	/*	R/W		1b: from NFC to I2C interface										*/
-	/*	In case the pass-through mode is NOT enabled, this bit should be set to 1b, otherwise there is no WRITE
-	access from the NFC perspective																					*/
-} AlxCrn120_0xFE_NcReg_0_TransferDir;
-
+{
+	I2cClockStr_NegAuthReached_StillPossible = 0b0,
+	I2cClockStr_NegAuthReached_Locked        = 0b1
+} AlxCrn120_Session_I2cClockStr_NegAuthReached;
 typedef enum
 {
-	SramMirrorDisable	= 0b0,	/*	R/W		0b: SRAM mirror disabled													*/
-	SramMirrorEnable	= 0b1	/*	R/W		1b: SRAM mirror enabled and mirrored SRAM starts at page SRAM_MIRROR_BLOCK	*/
-} AlxCrn120_0xFE_NcReg_1_SramMirrorOnOff;
-
-typedef enum
-{	/*defines the event upon which the signal output on the ED pin is pulled low*/
-	RFU							= 0b00, /*	R/W		00b: RFU																						*/
-	ValidStartOfCommunication	= 0b01,	/*	R/W		01b: by first valid start of communication (SoC)												*/
-	TagSelect					= 0b10, /*	R/W		10b: by selection of the tag																	*/
-	DataReadByNtcOrI2c			= 0b11	/*	R/W		11b: (in pass-through mode NFC-->I2C) if the data is ready to be read from the I2C interface	*/
-										/*			11b: (in pass-through mode I2C--> NFC) if the data is read by the NFC interface					*/
-} AlxCrn120_0xFE_NcReg_2_3_EdOn;
-
-typedef enum
-{	/*defines the event upon which the signal output on the FD pin is released*/
-	FieldOff							= 0b00, /*	R/W		00b: if the field is switched off																																						*/
-	FieldOffOrHaltOn					= 0b01, /*	R/W		01b: if the field is switched off or the tag is set to the HALT state																													*/
-	FieldOffOrReadLastPageNdef			= 0b10, /*	R/W		10b: if the field is switched off or the last page of the NDEF message has been read (defined in LAST_NDEF_BLOCK)																		*/
-	FieldOffOrReadLastPageByNfcOrI2c	= 0b11	/*	R/W		11b: (if ED_ON = 11b) if the field is switched off or if last data is read by I2C (in pass-through mode NFC ---> I2C) or last data is written by I2C (in pass-through mode I2C---> NFC)	*/
-										/*			11b: (if ED_ON = 00b or 01b or 10b) if the field is switched off																														*/
-} AlxCrn120_0xFE_NcReg_4_5_EdOff;
-
+	NsReg_RfFieldDetect_NotDetected = 0b0,
+	NsReg_RfFieldDetect_Detected    = 0b1
+} AlxCrn120_Session_NsReg_RfFieldPresent;
 typedef enum
 {
-	SramPassThroughModeDisable	= 0b0,	/*	R/W		0b: pass-through mode disabled													*/
-	SramPassThroughModeEnable	= 0b1	/*	R/W		1b: pass-through mode using SRAM enabled and SRAM mapped to end of Sector 0.	*/
-} AlxCrn120_0xFE_NcReg_6_PthruOnOff;
-
+	NsReg_EepromWriteBusy_Possible = 0b0,
+	NsReg_EepromWriteBusy_Busy     = 0b1
+} AlxCrn120_Session_NsReg_EepromWriteBusy;
 typedef enum
-{	/*Enables the NFC disable feature and enables soft reset through I2C repeated start*/
-	NfcSoftResetOn	= 0b0,	/*	R/W		0b: 	*/
-	NfcSoftResetOff	= 0b1	/*	R/W		1b: 	*/
-} AlxCrn120_0xFE_NcReg_7_NfcsI2cRstOnOff;
-
-	/**********			I2C_CLOCK_STR		*******/
-
+{
+	NsReg_EepromWriteErr_Ok    = 0b0,
+	NsReg_EepromWriteErr_Error = 0b1
+} AlxCrn120_Session_NsReg_EepromWriteErr;
 typedef enum
-{ /*	Enables (1b) or disable (0b) the I2C clock stretching	*/
-	ClockStretchingDisable	= 0b0, /*	R/W		0b:disable	*/
-	ClockStretchingEnable	= 0b1	/*	R/W		1b:Enables 	*/
-} AlxCrn120_0xFE_I2cClockStr_0_ClockStretching;
-
+{
+	NsReg_SramNfcReady_DataNotReady = 0b0,
+	NsReg_SramNfcReady_DataReady    = 0b1
+} AlxCrn120_Session_NsReg_SramNfcReady;
 typedef enum
-{ /*	Status bit to show the number of negative PWD_AUTH attempts reached	*/
-	AuthLockNotReached	= 0b0, /*	R/W		0b: PWD_AUTH still possible	*/
-	AuthLockReached		= 0b1	/*	R/W		1b: PWD_AUTH locked		 	*/
-} AlxCrn120_0xFE_I2cClockStr_1_NegAuthReached;
-
-
-
-	/**********		7	NS_REG		*******/
+{
+	NsReg_SramI2cReady_DataNotReady = 0b0,
+	NsReg_SramI2cReady_DataReady    = 0b1
+} AlxCrn120_Session_NsReg_SramI2cReady;
 typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	NfcFieldNotDetect	= 0b0, /*	R/W		0b: NFC field is not detected	*/
-	NfcFieldDetect		= 0b1	/*	R/W		1b: NFC field is detected		*/
-} AlxCrn120_0xFE_7_NsReg_0_RfFieldDetect;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	EepromWriteCyclePossible	= 0b0, /*	R/W		0b: EEPROM access possible											*/
-	EepromWriteCycleBusy		= 0b1	/*	R/W		1b: EEPROM write cycle in progress - access to EEPROM disabled		*/
-} AlxCrn120_0xFE_7_NsReg_1_EepromWriteCycle;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	EepromWriteOk		= 0b0,	/*	R/W		0b: Needs to be written back via I2C to 0b to be cleared 														*/
-	EepromWriteError	= 0b1	/*	R/W		1b: HV voltage error during EEPROM write or erase cycle.Needs to be written back via I2C to 0b to be cleared	*/
-} AlxCrn120_0xFE_7_NsReg_2_EepromWriteError;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	SramNfcNotReady = 0b0,	/*	R/W		0b:														*/
-	SramNfcReady	= 0b1	/*	R/W		1b: data is ready in SRAM buffer to be read by NFC		*/
-} AlxCrn120_0xFE_7_NsReg_3_SramNfcReady;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	SramI2cNotReady = 0b0,	/*	R/W		0b:														*/
-	SramI2cReady	= 0b1	/*	R/W		1b: data is ready in SRAM buffer to be read by I2C		*/
-} AlxCrn120_0xFE_7_NsReg_4_SramI2cReady;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	MemoryAccessNfcNotLocked	= 0b0, /*	R/W		0b:														*/
-	MemoryAccessNfcLocked		= 0b1	/*	R/W		1b: Memory access is locked to the NFC interface		*/
-} AlxCrn120_0xFE_7_NsReg_5_MemoryAccessNfcLocked;
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	MemoryAccessI2cNotLocked	= 0b0, /*	R/W		0b:														*/
-	MemoryAccessI2cLocked		= 0b1	/*	R/W		1b: Memory access is locked to the I2C interface		*/
-} AlxCrn120_0xFE_7_NsReg_6_MemoryAccessI2cLocked;
-
-	typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	NdefDataReadAll		= 0b0, /*	R/W		0b:														*/
-	NdefDataReadNotAll	= 0b1	/*	R/W		1b: all data bytes read from the address specified in LAST_NDEF_BLOCK. Bit is reset to 0b when read		*/
-} AlxCrn120_0xFE_7_NsReg_7_NdefDataRead;
-
-	/**********		8	REG_LOCK		*******/
+{
+	NsReg_MemNfcLocked_NotLocked	= 0b0,
+	NsReg_MemNfcLocked_Locked		= 0b1
+} AlxCrn120_Session_NsReg_MemNfcLocked;
 typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	NfcConfigLockEnabled	= 0b0, /*	R/W		0b: Configuration bytes may be changed via I2C														*/
-	NfcConfigLockDisable	= 0b1	/*	R/W		1b: Configuration bytes cannot be changed via I2C Once set to 1b, cannot be reset to 0b anymore. 	*/
-} AlxCrn120_0xFE_8_RegLock_Rfu_0_NfcConfigLock;
+{
+	NsReg_MemI2cLocked_NotLocked	= 0b0,
+	NsReg_MemI2cLocked_Locked		= 0b1
+} AlxCrn120_Session_NsReg_MemI2cLocked;
 typedef enum
-{ /*	I2C Configuration Lock Bit		*/
-	I2cConfigLockEnabled	= 0b0, /*	R/W		0b: Configuration bytes may be changed via I2C														*/
-	I2cConfigLockDisable	= 0b1	/*	R/W		1b: Configuration bytes cannot be changed via I2C Once set to 1b, cannot be reset to 0b anymore. 	*/
-} AlxCrn120_0xFE_8_RegLock_Rfu_1_I2cConfigLock;
+{
+	NsReg_NdefDataRead_Read    = 0b0,
+	NsReg_NdefDataRead_NotRead = 0b1
+} AlxCrn120_Session_NsReg_NdefDataRead;
 typedef union
 {
 	struct __attribute__((packed))
 	{
-		uint8_t NC_REG				: 8;	/* 	NC_REG									R/W		1 byte			*/
-		uint8_t LAST_NDEF_BLOCK		: 8;	/* 	LAST_NDEF_BLOCK							R/W		1 byte			*/
-		uint8_t SRAM_MIRROR_BLOCK	: 8;	/* 	SRAM_MIRROR_BLOCK						R/W		1 byte			*/
-		uint8_t WDT_LS				: 8;	/* 	WDT_LS									R/W		1 byte			*/
-		uint8_t WDT_MS				: 8;	/* 	WDT_MS									R/W		1 byte			*/
-		uint8_t I2C_CLOCK_STR		: 8;	/* 	I2C_CLOCK_STR							R/W		1 byte			*/
-		uint8_t NS_REG				: 8;	/* 	NS_REG									R/W		1 byte			*/
-		uint8_t RFU					: 8;	/* 	RFU										R/W		1 byte			*/
-		uint64_t _00h				: 64;	/* 	00h										R		8 bytes			*/
+		// #1 NC_REG
+		AlxCrn120_CfgSess_NcReg_TransferDir TRANSFER_DIR : 1;
+		AlxCrn120_CfgSess_NcReg_SramMirror SRAM_MIRROR : 1;
+		AlxCrn120_CfgSess_NcReg_EdOn ED_ON : 2;
+		AlxCrn120_CfgSess_NcReg_EdOff ED_OFF : 2;
+		AlxCrn120_CfgSess_NcReg_PassThroughMode PTHRU : 1;
+		AlxCrn120_CfgSess_NcReg_NfcsI2cRst NFCS_I2C_RST : 1;
+
+		// #2 LAST_NDEF_BLOCK
+		uint8_t LAST_NDEF_BLOCK : 8;
+
+		// #3 SRAM_MIRROR_BLOCK
+		uint8_t SRAM_MIRROR_BLOCK : 8;
+
+		// #4 WDT_LS
+		uint8_t WDT_LS : 8;
+
+		// #5 WDT_MS
+		uint8_t WDT_MS : 8;
+
+		// #6 I2C_CLOCK_STR
+		AlxCrn120_CfgSess_I2cClockStr_ClockStretching I2C_CLOCK_STR : 1;
+		AlxCrn120_Session_I2cClockStr_NegAuthReached NEG_AUTH_REACHED : 1;
+		uint8_t unused_2_7_RFU : 6; // bits 2..7 are unused
+
+		// #7 REG_LOCK
+		AlxCrn120_Session_NsReg_RfFieldPresent RF_FIELD_PRESENT : 1;
+		AlxCrn120_Session_NsReg_EepromWriteBusy EEPROM_WR_BUSY : 1;
+		AlxCrn120_Session_NsReg_EepromWriteErr EEPROM_WR_ERR : 1;
+		AlxCrn120_Session_NsReg_SramNfcReady SRAM_NFC_READY : 1;
+		AlxCrn120_Session_NsReg_SramI2cReady SRAM_I2C_READY : 1;
+		AlxCrn120_Session_NsReg_MemNfcLocked NFC_LOCKED : 1;
+		AlxCrn120_Session_NsReg_MemI2cLocked I2C_LOCKED : 1;
+		AlxCrn120_Session_NsReg_NdefDataRead NDEF_DATA_READ : 1;
+
+		// #8 RFU
+		uint8_t RFU : 8;
+
+		// #9 00
+		uint64_t _00h : 64;
 	};
 	uint8_t raw[16];
 } AlxCrn120_RegVal_FEh_SessionReg;
@@ -569,6 +665,10 @@ Alx_Status AlxCrn120_Init(AlxCrn120* me);
 Alx_Status AlxCrn120_DeInit(AlxCrn120* me);
 Alx_Status AlxCrn120_UsrMem(AlxCrn120* me, bool toRead, AlxCrn120_UsrMemAddr addr); // MF: Addr can be 0x01-0b37 and "9.7 READ and WRITE Operation" have to be used, meaning 16bytes in one read/write
 Alx_Status AlxCrn120_Sram(AlxCrn120* me, bool toRead, AlxCrn120_SramAddr addr); // MF: Addr can be 0xF8, 0xF9, 0xFA or 0xFB and "9.7 READ and WRITE Operation" have to be used, meaning 16bytes in one read/write
+
+// TODO - Capability Container (CC) ka jse bo tu kurblalo? treba nastudirat
+
+//Alx_Status AlxCrn120_LockPage(AlxCrn120* me, bool toLock, uint8_t* addr);	// MF: We won't use it at the moment
 
 
 #ifdef __cplusplus
