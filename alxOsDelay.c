566db28c2e7803cdd533bd3155554290717fcf94
Tomaz Vidovic <tomaz.vidovic@auralix.eu>
2022-04-14 15:45:28 +0200

    initial implementation of all ALX OS modules DONE

--- alxOsDelay.c
+++ alxOsDelay.c
@@ -1,6 +1,6 @@
 ﻿//******************************************************************************
-// @file alxOsMutex.c
-// @brief Auralix C Library - ALX OS Thread Module
+// @file alxOsDelay.c
+// @brief Auralix C Library - ALX OS Delay Module
 // @copyright Copyright (C) 2022 Auralix d.o.o. All rights reserved.
 //******************************************************************************
 
@@ -8,7 +8,7 @@
 //******************************************************************************
 // Includes
 //******************************************************************************
-#include "alxOsMutex.h"
+#include "alxOsDelay.h"
 
 
 //******************************************************************************
@@ -20,16 +20,14 @@
 //******************************************************************************
 // Constructor
 //******************************************************************************
-void AlxOsMutex_Ctor
+void AlxOsDelay_Ctor
 (
-	AlxOsMutex* me
+	AlxOsDelay* me,
+	AlxClk_Tick osTick
 )
 {
-	// Variables
-	#if defined(ALX_FREE_RTOS)
-	me->mutex = xSemaphoreCreateBinary();	// MF: Mutex is created once and won't be deleted during a program
-	xSemaphoreGive(me->mutex);
-	#endif
+	// Parameters
+	me->osTick = osTick;
 
 	// Info
 	me->wasCtorCalled = true;
@@ -39,27 +37,25 @@ void AlxOsMutex_Ctor
 //******************************************************************************
 // Functions
 //******************************************************************************
-void AlxOsMutex_Lock(AlxOsMutex* me)
+void AlxOsDelay_us(AlxOsDelay* me, uint64_t osDelay_us)
 {
-	// Lock Mutex
+	// #1 Assert
+	ALX_OS_DELAY_ASSERT(me->wasCtorCalled == true);
+	ALX_OS_DELAY_ASSERT(osDelay_us >= (2 * (uint64_t)me->osTick));
+	ALX_OS_DELAY_ASSERT((osDelay_us % (uint64_t)me->osTick) == 0);
+
+	// #2 Convert to osTick
+	uint64_t osDelay_osTick = osDelay_us / (uint64_t)me->osTick;
+
+	// #3 Delay
 	#if defined(ALX_FREE_RTOS)
-	xSemaphoreTake(me->mutex, portMAX_DELAY);
+	vTaskDelay(osDelay_osTick);
 	#endif
 }
-void AlxOsMutex_Unlock(AlxOsMutex* me)
-{
-	// Unlock Mutex
-	#if defined(ALX_FREE_RTOS)
-	xSemaphoreGive(me->mutex);
-	#endif
-}
-bool AlxOsMutex_IsMutexUnlocked(AlxOsMutex* me)
-{
-	// Get Status
-	#if defined(ALX_FREE_RTOS)
-	return uxSemaphoreGetCount(me->mutex);
-	#endif
-}
+void AlxOsDelay_ms(AlxOsDelay* me, uint64_t osDelay_ms)		{ AlxOsDelay_us(me, osDelay_ms * 1000); }
+void AlxOsDelay_sec(AlxOsDelay* me, uint64_t osDelay_sec)	{ AlxOsDelay_us(me, osDelay_sec * 1000000); }
+void AlxOsDelay_min(AlxOsDelay* me, uint64_t osDelay_min)	{ AlxOsDelay_us(me, osDelay_min * 60000000); }
+void AlxOsDelay_hr(AlxOsDelay* me, uint64_t osDelay_hr)		{ AlxOsDelay_us(me, osDelay_hr * 3600000000); }
 
 
 #endif // #if defined(ALX_FREE_RTOS)
---
+++ alxOsThread.h
@@ -0,0 +1,104 @@
+﻿//******************************************************************************
+// @file alxOsThread.h
+// @brief Auralix C Library - ALX OS Thread Module
+// @copyright Copyright (C) 2022 Auralix d.o.o. All rights reserved.
+//******************************************************************************
+
+#ifndef ALX_OS_THREAD_H
+#define ALX_OS_THREAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxGlobal.h"
+#include "alxAssert.h"
+#include "alxTrace.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_FREE_RTOS)
+
+
+//******************************************************************************
+// Preprocessor
+//******************************************************************************
+#define ALX_OS_THREAD_FILE "alxOsThread.h"
+
+// Assert //
+#if defined(_ALX_OS_THREAD_ASSERT_BKPT) || defined(_ALX_ASSERT_BKPT_ALL)
+	#define ALX_OS_THREAD_ASSERT(expr) ALX_ASSERT_BKPT(ALX_OS_THREAD_FILE, expr)
+#elif defined(_ALX_OS_THREAD_ASSERT_TRACE) || defined(_ALX_ASSERT_TRACE_ALL)
+	#define ALX_OS_THREAD_ASSERT(expr) ALX_ASSERT_TRACE(ALX_OS_THREAD_FILE, expr)
+#elif defined(_ALX_OS_THREAD_ASSERT_RST) || defined(_ALX_ASSERT_RST_ALL)
+	#define ALX_OS_THREAD_ASSERT(expr) ALX_ASSERT_RST(ALX_OS_THREAD_FILE, expr)
+#else
+	#define ALX_OS_THREAD_ASSERT(expr) do{} while (false)
+#endif
+
+// Trace //
+#if defined(_ALX_OS_THREAD_TRACE) || defined(_ALX_TRACE_ALL)
+	#define ALX_OS_THREAD_TRACE(...) ALX_TRACE_STD(ALX_OS_THREAD_FILE, __VA_ARGS__)
+#else
+	#define ALX_OS_THREAD_TRACE(...) do{} while (false)
+#endif
+
+
+//******************************************************************************
+// Types
+//******************************************************************************
+typedef struct
+{
+	#if defined(ALX_FREE_RTOS)
+	// Parameters
+	TaskFunction_t pxTaskCode;
+	const char* pcName;
+	configSTACK_DEPTH_TYPE usStackDepth;
+	void* pvParameters;
+	UBaseType_t uxPriority;
+	AlxClk_Tick osTick;
+
+	// Variables
+	TaskHandle_t* pxCreatedTask;
+	#endif
+
+	// Info
+	bool wasThreadStarted;
+	bool wasCtorCalled;
+} AlxOsThread;
+
+
+//******************************************************************************
+// Constructor
+//******************************************************************************
+#if defined(ALX_FREE_RTOS)
+void AlxOsThread_Ctor
+(
+	AlxOsThread* me,
+	TaskFunction_t pxTaskCode,
+	const char* const pcName,
+	const configSTACK_DEPTH_TYPE usStackDepth,
+	void* const pvParameters,
+	UBaseType_t uxPriority
+);
+#endif
+
+
+//******************************************************************************
+// Functions
+//******************************************************************************
+Alx_Status AlxOsThread_Start(AlxOsThread* me);
+
+
+#endif // #if defined(ALX_FREE_RTOS)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ALX_OS_THREAD_H
