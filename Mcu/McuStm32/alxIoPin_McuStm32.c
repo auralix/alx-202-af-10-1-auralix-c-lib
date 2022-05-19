1d0966696660018904a1dce786ef40ce316ba418
tomazvidovic <tomaz.vidovic@auralix.eu>
2022-05-19 21:15:50 +0200

    updated alx lib to newest master, then did all required changes for alxIoPin

--- Mcu/McuStm32/alxIoPin_McuStm32.c
+++ Mcu/McuStm32/alxIoPin_McuStm32.c
@@ -31,9 +31,9 @@ void AlxIoPin_Ctor
 	uint32_t mode,
 	uint32_t pull,
 	uint32_t speed,
-#if defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
+	#if defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
 	uint32_t alternate,
-#endif // defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
+	#endif // defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
 	bool val
 )
 {
@@ -46,38 +46,31 @@ void AlxIoPin_Ctor
 	me->igpio.Mode = mode;
 	me->igpio.Pull = pull;
 	me->igpio.Speed = speed;
-#if defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
-	me->igpio.Alternate = alternate;	  
-#endif // defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
+	#if defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
+	me->igpio.Alternate = alternate;
+	#endif // defined(ALX_STM32F4) || defined(ALX_STM32G4) || defined(ALX_STM32L0)
 
 	// Info
 	me->isInit = false;
 	me->wasCtorCalled = true;
-	me->isAssertOn = true;	// ON by default
 }
-void AlxIoPin_Init(AlxIoPin* me) // TODO: implement NVIC_EnableIRQ(EXTIx_IRQn), take into account pin number..
+void AlxIoPin_Init(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == false);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == false);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	// #1 Set isInit attribute
 	me->isInit = true;
 
 	// #2 Init GPIO
-	AlxIoPin_Write(me, me->val);  // Set initial output value, before config
+	AlxIoPin_Write(me, me->val);	// Set initial output value, before config
 	HAL_GPIO_Init((GPIO_TypeDef*)me->port, &me->igpio);
-	AlxIoPin_Write(me, me->val);  // Set initial output value, after config
+	AlxIoPin_Write(me, me->val);	// Set initial output value, after config
 }
 void AlxIoPin_DeInit(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	// #1 DeInit GPIO
 	HAL_GPIO_DeInit((GPIO_TypeDef*)me->port, me->igpio.Pin);
@@ -87,101 +80,90 @@ void AlxIoPin_DeInit(AlxIoPin* me)
 }
 bool AlxIoPin_Read(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	return HAL_GPIO_ReadPin((GPIO_TypeDef*)me->port, me->igpio.Pin);
 }
 void AlxIoPin_Write(AlxIoPin* me, bool val)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
-	(void)val;
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	HAL_GPIO_WritePin((GPIO_TypeDef*)me->port, me->igpio.Pin, (GPIO_PinState)val);
 }
 void AlxIoPin_Set(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	HAL_GPIO_WritePin((GPIO_TypeDef*)me->port, me->igpio.Pin, (GPIO_PinState)true);
 }
 void AlxIoPin_Reset(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	HAL_GPIO_WritePin((GPIO_TypeDef*)me->port, me->igpio.Pin, (GPIO_PinState)false);
 }
 void AlxIoPin_Toggle(AlxIoPin* me)
 {
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == true);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
 	HAL_GPIO_TogglePin((GPIO_TypeDef*)me->port, me->igpio.Pin);
 }
-void AlxIoPin_Lock(AlxIoPin* me)
+AlxIoPin_TriState AlxIoPin_Read_TriState(AlxIoPin* me)
 {
-	// TODO
-	ALX_IO_PIN_ASSERT(false);
-}
-void AlxIoPin_IrqHandler(AlxIoPin* me)
-{
-	HAL_GPIO_EXTI_IRQHandler(me->igpio.Pin);   // We will not use HAL_GPIO_EXTI_Callback, IRQ code should be directly placed in IRQ routine
-}
-void AlxIoPin_Config_PullNone(AlxIoPin* me)
-{
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == false);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	// Assert
+	ALX_IO_PIN_ASSERT(me->isInit == true);
+	ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
 
-	me->igpio.Pull = GPIO_NOPULL;
-}
-void AlxIoPin_Config_PullUp(AlxIoPin* me)
-{
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == false);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	//------------------------------------------------------------------------------
+	// #1 Read @ PullUp
+	//------------------------------------------------------------------------------
 
-	me->igpio.Pull = GPIO_PULLUP;
-}
-void AlxIoPin_Config_PullDown(AlxIoPin* me)
-{
-	if (me->isAssertOn)
-	{
-		ALX_IO_PIN_ASSERT(me->isInit == false);
-		ALX_IO_PIN_ASSERT(me->wasCtorCalled == true);
-	}
+	// Config PullUp
+	LL_GPIO_SetPinPull((GPIO_TypeDef*)me->port, me->igpio.Pin, LL_GPIO_PULL_UP);
 
-	me->igpio.Pull = GPIO_PULLDOWN;
-}
-void AlxIoPin_Config_AssertOn(AlxIoPin* me)
-{
-	me->isAssertOn = true;
-}
-void AlxIoPin_Config_AssertOff(AlxIoPin* me)
-{
-	me->isAssertOn = false;
+	// Wait
+	AlxDelay_ms(1);
+
+	// Read
+	bool valPullUp = AlxIoPin_Read(me);
+
+	//------------------------------------------------------------------------------
+	// #2 Read @ PullDown
+	//------------------------------------------------------------------------------
+
+	// Config PullDown
+	LL_GPIO_SetPinPull((GPIO_TypeDef*)me->port, me->igpio.Pin, LL_GPIO_PULL_DOWN);
+
+	// Wait
+	AlxDelay_ms(1);
+
+	// Read
+	bool valPullDown = AlxIoPin_Read(me);
+
+	//------------------------------------------------------------------------------
+	// #3 Handle Return
+	//------------------------------------------------------------------------------
+	if ((valPullUp == true) && (valPullDown == false))
+	{
+		return AlxIoPin_TriState_HiZ;
+	}
+	else if ((valPullUp == true) && (valPullDown == true))
+	{
+		return AlxIoPin_TriState_Hi;
+	}
+	else if ((valPullUp == false) && (valPullDown == false))
+	{
+		return AlxIoPin_TriState_Lo;
+	}
+	else
+	{
+		return AlxIoPin_TriState_Undefined;
+	}
 }
 
 #endif
