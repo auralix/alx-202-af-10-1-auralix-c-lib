920d59698b1706322e03c6e56a744bee8b7c582b
tomazvidovic <tomaz@tomazvidovic.com>
2024-07-18 19:02:16 +0200

    alxTrace_McuZephyr.h implemented

---
+++ Mcu/McuZephyr/alxTrace_McuZephyr.c
@@ -0,0 +1,128 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxTrace_McuZephyr.c
+  * @brief		Auralix C Library - ALX Trace MCU Zephyr Module
+  * @copyright	Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxTrace_McuZephyr.h"
+#include "alxTrace.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_C_LIB) && defined(ALX_ZEPHYR)
+
+
+//******************************************************************************
+// Constructor
+//******************************************************************************
+
+/**
+  * @brief
+  * @param[in,out]	me
+  * @param[in]		port
+  * @param[in]		pin
+  * @param[in]		alternate
+  * @param[in]		uart
+  * @param[in]		baudRate
+  */
+void AlxTrace_Ctor
+(
+	AlxTrace* me
+)
+{
+	// Variables
+	me->uartDevice = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+	// Info
+	me->wasCtorCalled = true;
+	me->isInit = false;
+}
+
+
+//******************************************************************************
+// Functions
+//******************************************************************************
+
+/**
+  * @brief
+  * @param[in,out]	me
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxTrace_Init(AlxTrace* me)
+{
+	// Check
+	if (me->uartDevice == NULL)
+	{
+		return Alx_Err;
+	}
+
+	// Set isInit
+	me->isInit = true;
+
+	// Return
+	return Alx_Ok;
+}
+
+/**
+  * @brief
+  * @param[in,out]	me
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxTrace_DeInit(AlxTrace* me)
+{
+	// Clear isInit
+	me->isInit = false;
+
+	// Return
+	return Alx_Ok;
+}
+
+/**
+  * @brief
+  * @param[in,out]	me
+  * @param[in]		str
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxTrace_WriteStr(AlxTrace* me, const char* str)
+{
+	// Write
+	while (*str)
+	{
+		uart_poll_out(me->uartDevice, *str++);
+	}
+
+	// Return
+	return Alx_Ok;
+}
+
+
+#endif	// #if defined(ALX_C_LIB) && defined(ALX_ZEPHYR)
---
+++ Mcu/McuZephyr/alxTrace_McuZephyr.h
@@ -0,0 +1,80 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxTrace_McuZephyr.h
+  * @brief		Auralix C Library - ALX Trace MCU Zephyr Module
+  * @copyright	Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Include Guard
+//******************************************************************************
+#ifndef ALX_TRACE_MCU_ZEPHYR_H
+#define ALX_TRACE_MCU_ZEPHYR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxGlobal.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_C_LIB) && defined(ALX_ZEPHYR)
+
+
+//******************************************************************************
+// Types
+//******************************************************************************
+typedef struct
+{
+	// Variables
+	const struct device* uartDevice;
+
+	// Info
+	bool wasCtorCalled;
+	bool isInit;
+} AlxTrace;
+
+
+//******************************************************************************
+// Constructor
+//******************************************************************************
+void AlxTrace_Ctor
+(
+	AlxTrace* me
+);
+
+
+#endif	// #if defined(ALX_C_LIB) && defined(ALX_ZEPHYR)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// #ifndef ALX_TRACE_MCU_ZEPHYR_H
