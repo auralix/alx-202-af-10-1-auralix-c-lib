f99e61ef1233d473c397c283f7ca33c7c48a9b2d
Žan Škorja <113106345+zanskorja@users.noreply.github.com>
2025-02-10 13:46:39 +0100

    Update alxSerialPortLogger.py

--- alxSerialPortLogger.py
+++ alxSerialPortLogger.py
@@ -161,7 +161,7 @@ class SerialPortLogger:
 								if self.__fifoQueue.qsize() > 10:
 									self.__fifoQueue.get()
 
-						buffer = b""
+						buffer = ""
 		except Exception as e:
 			self.__logger.fatal(f"FAIL: {e}")
 			logging.fatal(f"FAIL: {e}")
