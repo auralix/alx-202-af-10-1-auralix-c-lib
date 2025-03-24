323027b0d2b26044af49e434d55557394fbf5d59
tomazvidovic <tomaz@tomazvidovic.com>
2025-03-24 16:00:44 +0100

    created empty files + defined interface & parameters

---
+++ alxFtp.c
@@ -0,0 +1,153 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxFtp.c
+  * @brief		Auralix C Library - ALX FTP Module
+  * @copyright	Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxFtp.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_C_LIB)
+
+
+//******************************************************************************
+// Constructor
+//******************************************************************************
+void AlxFtp_Ctor
+(
+	AlxFtp* me,
+	AlxNet* alxNet,
+	const char* serverAddr,
+	bool serverAddrIsHostname,
+	uint16_t serverPort,
+	const char* clientUsername,
+	const char* clientPassword
+)
+{
+	// Parameters
+	me->alxNet = alxNet;
+	me->serverAddr = serverAddr;
+	me->serverAddrIsHostname = serverAddrIsHostname;
+	me->serverPort = serverPort;
+	me->clientUsername = clientUsername;
+	me->clientPassword = clientPassword;
+
+	// Variables
+
+	// Info
+	me->wasCtorCalled = true;
+	me->isClientLoggedIn = false;
+}
+
+
+//******************************************************************************
+// Fields
+//******************************************************************************
+void AlxFtp_SetServerAddr(AlxFtp* me, const char* serverAddr)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+
+	// Set
+	me->serverAddr = serverAddr;
+}
+void AlxFtp_SetServerAddrIsHostname(AlxFtp* me, bool serverAddrIsHostname)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+
+	// Set
+	me->serverAddrIsHostname = serverAddrIsHostname;
+}
+void AlxFtp_SetServerPort(AlxFtp* me, uint16_t serverPort)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+
+	// Set
+	me->serverPort = serverPort;
+}
+void AlxFtp_SetClientUsername(AlxFtp* me, const char* clientUsername)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+
+	// Set
+	me->clientUsername = clientUsername;
+}
+void AlxFtp_SetClientPassword(AlxFtp* me, const char* clientPassword)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+
+	// Set
+	me->clientPassword = clientPassword;
+}
+
+
+//******************************************************************************
+// Functions
+//******************************************************************************
+Alx_Status AlxFtp_Client_Login(AlxFtp* me)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+	ALX_FTP_ASSERT(me->isClientLoggedIn == false);
+
+	// Set
+	me->isClientLoggedIn = true;
+
+	// Return
+	return Alx_Ok;
+}
+Alx_Status AlxFtp_Client_Logout(AlxFtp* me)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+	ALX_FTP_ASSERT(me->isClientLoggedIn == true);
+
+	// Clear
+	me->isClientLoggedIn = false;
+
+	// Return
+	return Alx_Ok;
+}
+Alx_Status AlxFtp_Client_UploadFile(AlxFtp* me, const char* localFilePath, const char* remoteFilePath)
+{
+	// Assert
+	ALX_FTP_ASSERT(me->wasCtorCalled == true);
+	ALX_FTP_ASSERT(me->isClientLoggedIn == true);
+
+	// Return
+	return Alx_Ok;
+}
+
+
+#endif	// #if defined(ALX_C_LIB)
---
+++ alxFtp.h
@@ -0,0 +1,141 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxFtp.h
+  * @brief		Auralix C Library - ALX FTP Module
+  * @copyright	Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Include Guard
+//******************************************************************************
+#ifndef ALX_FTP_H
+#define ALX_FTP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//******************************************************************************
+// Includes
+//******************************************************************************rp
+#include "alxGlobal.h"
+#include "alxTrace.h"
+#include "alxAssert.h"
+#include "alxNet.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_C_LIB)
+
+
+//******************************************************************************
+// Preprocessor
+//******************************************************************************
+#define ALX_FTP_FILE "alxFtp.h"
+
+// Assert //
+#if defined(ALX_FTP_ASSERT_BKPT_ENABLE)
+	#define ALX_FTP_ASSERT(expr) ALX_ASSERT_BKPT(ALX_FTP_FILE, expr)
+#elif defined(ALX_FTP_ASSERT_TRACE_ENABLE)
+	#define ALX_FTP_ASSERT(expr) ALX_ASSERT_TRACE(ALX_FTP_FILE, expr)
+#elif defined(ALX_FTP_ASSERT_RST_ENABLE)
+	#define ALX_FTP_ASSERT(expr) ALX_ASSERT_RST(ALX_FTP_FILE, expr)
+#else
+	#define ALX_FTP_ASSERT(expr) do{} while (false)
+#endif
+
+// Trace //
+#if defined(ALX_FTP_TRACE_ENABLE)
+	#define ALX_FTP_TRACE_WRN(...) ALX_TRACE_WRN(ALX_FTP_FILE, __VA_ARGS__)
+#else
+	#define ALX_FTP_TRACE_WRN(...) do{} while (false)
+#endif
+
+
+//******************************************************************************
+// Types
+//******************************************************************************
+typedef struct
+{
+	// Defines
+	#define ALX_FTP_BUFF_LEN 1460
+
+	// Parameters
+	AlxNet* alxNet;
+	const char* serverAddr;
+	bool serverAddrIsHostname;
+	uint16_t serverPort;
+	const char* clientUsername;
+	const char* clientPassword;
+
+	// Variables
+
+	// Info
+	bool wasCtorCalled;
+	bool isClientLoggedIn;
+} AlxFtp;
+
+
+//******************************************************************************
+// Constructor
+//******************************************************************************
+void AlxFtp_Ctor
+(
+	AlxFtp* me,
+	AlxNet* alxNet,
+	const char* serverAddr,
+	bool serverAddrIsHostname,
+	uint16_t serverPort,
+	const char* clientUsername,
+	const char* clientPassword
+);
+
+
+//******************************************************************************
+// Fields
+//******************************************************************************
+void AlxFtp_SetServerAddr(AlxFtp* me, const char* serverAddr);
+void AlxFtp_SetServerAddrIsHostname(AlxFtp* me, bool serverAddrIsHostname);
+void AlxFtp_SetServerPort(AlxFtp* me, uint16_t serverPort);
+void AlxFtp_SetClientUsername(AlxFtp* me, const char* clientUsername);
+void AlxFtp_SetClientPassword(AlxFtp* me, const char* clientPassword);
+
+
+//******************************************************************************
+// Functions
+//******************************************************************************
+Alx_Status AlxFtp_Client_Login(AlxFtp* me);
+Alx_Status AlxFtp_Client_Logout(AlxFtp* me);
+Alx_Status AlxFtp_Client_UploadFile(AlxFtp* me, const char* localFilePath, const char* remoteFilePath);
+
+
+#endif	// #if defined(ALX_C_LIB)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// #ifndef ALX_FTP_H
