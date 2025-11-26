9121ac4fda0209db8207287b1e585cc5bdc990c9
tomazvidovic <tomaz.vidovic@auralix.eu>
2025-11-26 10:39:32 +0100

    EA15-39: alxUsb module initial creation with chatGPT

---
+++ alxUsb.c
@@ -0,0 +1,367 @@
+﻿/**
+  ******************************************************************************
+  * @file        alxUsb.c
+  * @brief       Auralix C Library - ALX USB Host MSC Module
+  * @copyright   Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//*******************************************************************************
+// Includes
+//*******************************************************************************
+#include "alxUsb.h"
+
+//*******************************************************************************
+// Module Guard
+//*******************************************************************************
+#if defined(ALX_C_LIB)
+
+//*******************************************************************************
+// Local function prototypes
+//*******************************************************************************
+
+static void AlxUsb_UserProcess(USBH_HandleTypeDef* phost, uint8_t id);
+static void AlxUsb_StatusChange_Callback(AlxUsb* me, AlxUsb_State newState);
+
+//*******************************************************************************
+// Local variables
+//*******************************************************************************
+
+// NOTE: current implementation assumes a single USB host instance.
+// If multiple hosts are needed, this can be extended.
+static AlxUsb* s_alxUsbInstance = NULL;
+
+
+//*******************************************************************************
+// Constructor
+//*******************************************************************************
+void AlxUsb_Ctor
+(
+	AlxUsb*	me,
+	uint8_t	hostId
+)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+
+	// Parameters
+	me->hostId = hostId;
+
+	// Info
+	me->pendingEvent  = 0U;
+	me->state         = AlxUsb_State_NoDevice;
+	me->wasCtorCalled = true;
+	me->isInit        = false;
+}
+
+
+//*******************************************************************************
+// Functions
+//*******************************************************************************
+
+/**
+  * @brief
+  * @param[in,out]	me	Context
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxUsb_Init(AlxUsb* me)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == false);
+
+	// Store global instance pointer
+	s_alxUsbInstance = me;
+
+	// Clear internal state
+	me->pendingEvent = 0U;
+	me->state        = AlxUsb_State_NoDevice;
+
+	// Init USB Host Core
+	if (USBH_Init(&me->host, AlxUsb_UserProcess, me->hostId) != USBH_OK)
+	{
+		ALX_USB_TRACE_WRN("USBH_Init failed");
+		return Alx_Err;
+	}
+
+	// Register MSC Class
+	if (USBH_RegisterClass(&me->host, USBH_MSC_CLASS) != USBH_OK)
+	{
+		ALX_USB_TRACE_WRN("USBH_RegisterClass MSC failed");
+		return Alx_Err;
+	}
+
+	// Start Host
+	if (USBH_Start(&me->host) != USBH_OK)
+	{
+		ALX_USB_TRACE_WRN("USBH_Start failed");
+		return Alx_Err;
+	}
+
+	// Info
+	me->isInit = true;
+
+	// Return
+	return Alx_Ok;
+}
+
+/**
+  * @brief
+  * @param[in,out]	me	Context
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxUsb_DeInit(AlxUsb* me)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == true);
+
+	// Stop + DeInit USB Host
+	(void)USBH_Stop(&me->host);
+	(void)USBH_DeInit(&me->host);
+
+	// Clear instance
+	if (s_alxUsbInstance == me)
+	{
+		s_alxUsbInstance = NULL;
+	}
+
+	me->isInit        = false;
+	me->pendingEvent  = 0U;
+	me->state         = AlxUsb_State_NoDevice;
+
+	return Alx_Ok;
+}
+
+/**
+  * @brief			Drive USB host state machine (polling).
+  * @param[in,out]	me	Context
+  */
+void AlxUsb_Handle(AlxUsb* me)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == true);
+
+	// Drive USB Host core
+	USBH_Process(&me->host);
+
+	// Consume event from callback
+	uint8_t ev = me->pendingEvent;
+	me->pendingEvent = 0U;
+
+	if (ev != 0U)
+	{
+		AlxUsb_State oldState = me->state;
+
+		switch (ev)
+		{
+			case HOST_USER_CONNECTION:
+				me->state = AlxUsb_State_Connected;
+				break;
+
+			case HOST_USER_CLASS_SELECTED:
+				me->state = AlxUsb_State_Enumerating;
+				break;
+
+			case HOST_USER_CLASS_ACTIVE:
+				me->state = AlxUsb_State_Ready;
+				break;
+
+			case HOST_USER_DISCONNECTION:
+				me->state = AlxUsb_State_Disconnected;
+				break;
+
+			case HOST_USER_UNRECOVERED_ERROR:
+				me->state = AlxUsb_State_Error;
+				// Optional: you could try auto-restart here
+				// USBH_Stop(&me->host);
+				// USBH_Start(&me->host);
+				break;
+
+			case HOST_USER_SELECT_CONFIGURATION:
+			default:
+				// Not used in this abstraction
+				break;
+		}
+
+		if (me->state != oldState)
+		{
+			AlxUsb_StatusChange_Callback(me, me->state);
+		}
+	}
+
+	// Extra safety: if we think we are ready but MSC is not, downgrade state
+	if (me->state == AlxUsb_State_Ready)
+	{
+		if (USBH_MSC_IsReady(&me->host) != USBH_OK)
+		{
+			me->state = AlxUsb_State_Enumerating;
+		}
+	}
+}
+
+/**
+  * @brief
+  * @param[in,out]	me	Context
+  * @retval			true	MSC device ready for block I/O
+  * @retval			false	Not ready / no device / error
+  */
+bool AlxUsb_IsReady(AlxUsb* me)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == true);
+
+	if (me->state != AlxUsb_State_Ready)
+	{
+		return false;
+	}
+
+	// Double-check with MSC class driver
+	return (USBH_MSC_IsReady(&me->host) == USBH_OK);
+}
+
+/**
+  * @brief
+  * @param[in,out]	me		Context
+  * @param[in]		sector	Start sector (LBA)
+  * @param[in]		count	Number of sectors to read
+  * @param[out]		buff	Destination buffer
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxUsb_Read
+(
+	AlxUsb*		me,
+	uint32_t	sector,
+	uint32_t	count,
+	uint8_t*	buff
+)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == true);
+	ALX_USB_ASSERT(buff != NULL);
+	ALX_USB_ASSERT(count > 0U);
+
+	if (!AlxUsb_IsReady(me))
+	{
+		ALX_USB_TRACE_WRN("Read requested while not ready");
+		return Alx_Err;
+	}
+
+	USBH_StatusTypeDef st = USBH_MSC_Read(&me->host, 0U, sector, buff, count);
+
+	if (st == USBH_OK)
+	{
+		return Alx_Ok;
+	}
+
+	ALX_USB_TRACE_WRN("USBH_MSC_Read failed (%d)", (int)st);
+	return Alx_Err;
+}
+
+/**
+  * @brief
+  * @param[in,out]	me		Context
+  * @param[in]		sector	Start sector (LBA)
+  * @param[in]		count	Number of sectors to write
+  * @param[in]		buff	Source buffer
+  * @retval			Alx_Ok
+  * @retval			Alx_Err
+  */
+Alx_Status AlxUsb_Write
+(
+	AlxUsb*			me,
+	uint32_t		sector,
+	uint32_t		count,
+	const uint8_t*	buff
+)
+{
+	// Assert
+	ALX_USB_ASSERT(me != NULL);
+	ALX_USB_ASSERT(me->wasCtorCalled == true);
+	ALX_USB_ASSERT(me->isInit == true);
+	ALX_USB_ASSERT(buff != NULL);
+	ALX_USB_ASSERT(count > 0U);
+
+	if (!AlxUsb_IsReady(me))
+	{
+		ALX_USB_TRACE_WRN("Write requested while not ready");
+		return Alx_Err;
+	}
+
+	USBH_StatusTypeDef st =
+		USBH_MSC_Write(&me->host, 0U, sector, (uint8_t*)buff, count);
+
+	if (st == USBH_OK)
+	{
+		return Alx_Ok;
+	}
+
+	ALX_USB_TRACE_WRN("USBH_MSC_Write failed (%d)", (int)st);
+	return Alx_Err;
+}
+
+
+//*******************************************************************************
+// Local functions
+//*******************************************************************************
+
+/**
+  * @brief	USB Host core user callback (bridge to AlxUsb)
+  */
+static void AlxUsb_UserProcess(USBH_HandleTypeDef* phost, uint8_t id)
+{
+	(void)phost;
+
+	if (s_alxUsbInstance != NULL)
+	{
+		// Just record, actual handling is done in AlxUsb_Handle()
+		s_alxUsbInstance->pendingEvent = id;
+	}
+}
+
+/**
+  * @brief	Status change hook, can be used for debug/LED/etc.
+  * @note	Default implementation: do nothing.
+  *			If you want board-specific behaviour, you can modify this
+  *			function in this file as needed.
+  */
+static void AlxUsb_StatusChange_Callback(AlxUsb* me, AlxUsb_State newState)
+{
+	(void)me;
+	(void)newState;
+	// Example:
+	// ALX_USB_TRACE_WRN("USB state = %d", (int)newState);
+}
+
+
+#endif	// #if defined(ALX_C_LIB)
---
+++ alxUsb.h
@@ -0,0 +1,209 @@
+﻿/**
+  ******************************************************************************
+  * @file        alxUsb.h
+  * @brief       Auralix C Library - ALX USB Host MSC Module
+  * @copyright   Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C Library.
+  *
+  * Auralix C Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//*******************************************************************************
+// Include Guard
+//*******************************************************************************
+#ifndef ALX_USB_H
+#define ALX_USB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//*******************************************************************************
+// Includes
+//*******************************************************************************
+#include "alxGlobal.h"
+#include "alxTrace.h"
+#include "alxAssert.h"
+
+#include "usbh_core.h"
+#include "usbh_msc.h"
+
+//*******************************************************************************
+// Module Guard
+//*******************************************************************************
+#if defined(ALX_C_LIB)
+
+//*******************************************************************************
+// Preprocessor
+//*******************************************************************************
+#define ALX_USB_FILE "alxUsb.h"
+
+// Assert //
+#if defined(ALX_USB_ASSERT_BKPT_ENABLE)
+	#define ALX_USB_ASSERT(expr)	ALX_ASSERT_BKPT(ALX_USB_FILE, expr)
+#elif defined(ALX_USB_ASSERT_TRACE_ENABLE)
+	#define ALX_USB_ASSERT(expr)	ALX_ASSERT_TRACE(ALX_USB_FILE, expr)
+#elif defined(ALX_USB_ASSERT_RST_ENABLE)
+	#define ALX_USB_ASSERT(expr)	ALX_ASSERT_RST(ALX_USB_FILE, expr)
+#else
+	#define ALX_USB_ASSERT(expr)	do {} while (false)
+#endif
+
+// Trace //
+#if defined(ALX_USB_TRACE_WRN_ENABLE)
+	#define ALX_USB_TRACE_WRN(...)	ALX_TRACE_WRN(ALX_USB_FILE, __VA_ARGS__)
+#else
+	#define ALX_USB_TRACE_WRN(...)	do {} while (false)
+#endif
+
+//*******************************************************************************
+// Types
+//*******************************************************************************
+
+/**
+ * @brief High-level USB MSC host state
+ *
+ * This is internal to the module; external users normally only call
+ * AlxUsb_IsReady().
+ */
+typedef enum
+{
+	AlxUsb_State_NoDevice = 0,
+	AlxUsb_State_Connected,
+	AlxUsb_State_Enumerating,
+	AlxUsb_State_Ready,
+	AlxUsb_State_Disconnected,
+	AlxUsb_State_Error,
+} AlxUsb_State;
+
+/**
+ * @brief ALX USB MSC context
+ */
+typedef struct
+{
+	// Parameters
+	uint8_t				hostId;			///< USB host ID passed to USBH_Init (e.g. HOST_FS)
+
+	// Low-level USB Host handle (owned by this module)
+	USBH_HandleTypeDef	host;
+
+	// Internal state
+	volatile uint8_t	pendingEvent;	///< Last HOST_USER_* event (0 = none)
+	AlxUsb_State		state;			///< Current high-level state
+
+	// Info
+	bool				wasCtorCalled;
+	bool				isInit;
+} AlxUsb;
+
+
+//*******************************************************************************
+// Constructor
+//*******************************************************************************
+
+/**
+  * @brief		Constructor
+  * @param[in]	me		Context
+  * @param[in]	hostId	Host ID passed to USBH_Init (e.g. HOST_FS)
+  */
+void AlxUsb_Ctor
+(
+	AlxUsb*	me,
+	uint8_t	hostId
+);
+
+
+//*******************************************************************************
+// Functions
+//*******************************************************************************
+
+/**
+  * @brief	Initialize USB Host + MSC class and start host.
+  * @param	me		Context
+  * @retval	Alx_Ok
+  * @retval	Alx_Err
+  */
+Alx_Status AlxUsb_Init(AlxUsb* me);
+
+/**
+  * @brief	DeInitialize USB Host.
+  * @param	me		Context
+  * @retval	Alx_Ok
+  * @retval	Alx_Err
+  */
+Alx_Status AlxUsb_DeInit(AlxUsb* me);
+
+/**
+  * @brief	Drive USB Host state machine (polling).
+  *			Must be called regularly (e.g. in main loop).
+  * @param	me		Context
+  */
+void AlxUsb_Handle(AlxUsb* me);
+
+/**
+  * @brief	Check if MSC device is ready for block I/O.
+  * @param	me		Context
+  * @retval	true	Device is ready (enumerated + MSC ready)
+  * @retval	false	Not ready / no device / error
+  */
+bool AlxUsb_IsReady(AlxUsb* me);
+
+/**
+  * @brief	Read sectors from USB MSC device.
+  * @param	me		Context
+  * @param	sector	Start sector (LBA)
+  * @param	count	Number of sectors to read
+  * @param	buff	Destination buffer
+  * @retval	Alx_Ok
+  * @retval	Alx_Err
+  */
+Alx_Status AlxUsb_Read
+(
+	AlxUsb*		me,
+	uint32_t	sector,
+	uint32_t	count,
+	uint8_t*	buff
+);
+
+/**
+  * @brief	Write sectors to USB MSC device.
+  * @param	me		Context
+  * @param	sector	Start sector (LBA)
+  * @param	count	Number of sectors to write
+  * @param	buff	Source buffer
+  * @retval	Alx_Ok
+  * @retval	Alx_Err
+  */
+Alx_Status AlxUsb_Write
+(
+	AlxUsb*			me,
+	uint32_t		sector,
+	uint32_t		count,
+	const uint8_t*	buff
+);
+
+
+#endif	// #if defined(ALX_C_LIB)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// #ifndef ALX_USB_H
