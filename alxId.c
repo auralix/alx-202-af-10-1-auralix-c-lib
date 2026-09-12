183586d61f9669fdc2871cb9c2e3bae7e1d6d791
tomazvidovic <tomaz.vidovic@auralix.eu>
2026-09-12 19:42:55 +0200

    ALX-1553 in_prog

--- alxId.c
+++ alxId.c
@@ -217,7 +217,7 @@ void AlxId_Ctor
 	//------------------------------------------------------------------------------
 	#if defined(__clang__)
 		// Preprocessor
-		#if (ALX_xVER3(__clang_major__, __clang_minor__, __clang_patchlevel__) < ALX_COMP_VER_MIN_REQUIRED)
+		#if (ALX_xVER3(__clang_major__, __clang_minor__, __clang_patchlevel__) < ALX_COMP_CLANG_VER_MIN_REQUIRED)
 			#error
 		#endif
 
@@ -227,12 +227,12 @@ void AlxId_Ctor
 		me->fwComp.verMinor = __clang_minor__;
 		me->fwComp.verPatch = __clang_patchlevel__;
 		me->fwComp.ver = ALX_xVER3(__clang_major__, __clang_minor__, __clang_patchlevel__);
-		me->fwComp.verMinRequired = ALX_COMP_VER_MIN_REQUIRED;
+		me->fwComp.verMinRequired = ALX_COMP_CLANG_VER_MIN_REQUIRED;
 	#elif defined(__GNUC__)
 		// Preprocessor
 		// GNU -> [G]NU is [N]ot [U]nix
 		// GCC -> [G]NU [C]ompiler [C]ollection
-		#if (ALX_xVER3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) < ALX_COMP_VER_MIN_REQUIRED)
+		#if (ALX_xVER3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) < ALX_COMP_GCC_VER_MIN_REQUIRED)
 			#error
 		#endif
 
@@ -242,7 +242,7 @@ void AlxId_Ctor
 		me->fwComp.verMinor = __GNUC_MINOR__;
 		me->fwComp.verPatch = __GNUC_PATCHLEVEL__;
 		me->fwComp.ver = ALX_xVER3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-		me->fwComp.verMinRequired = ALX_COMP_VER_MIN_REQUIRED;
+		me->fwComp.verMinRequired = ALX_COMP_GCC_VER_MIN_REQUIRED;
 	#else
 		strcpy(me->fwComp.name, "Unknown");
 	#endif
